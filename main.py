1ca9fa9357022b8ee90d198e9b732535bec00e15
Pedro Maia <pedroabmaia@hotmail.com>
2023-10-09 17:21:13 +0100

    Implemented the functionalities of word and character pressing  And also the ending of the program

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
 #import section
-import argparse
-import readchar
+import argparse #arguments
+import readchar #read keys
 
-from program_run import runProgram
-from readchar import readkey, key
+from program_run import runProgram #function to not pollute the main
+from readchar import readkey, key #read keys
 
 #Main running code
 def main():
+
     #Principal elements of the help text
     parser = argparse.ArgumentParser( 
                     prog='Typing Velocity Test',
@@ -16,34 +17,43 @@ def main():
                     epilog='Pedro Maia, Salomé Dias, Vitor Gomes. UA PSR - 23/24')
     
     #Arguments for the program: 'time mode', 'max value' and 'use words'
-    parser.add_argument('-utm', '--use_time_mode', type=bool, help='option to use time mode insted of number of inputs (default)')
+    parser.add_argument('-utm', '--use_time_mode', help='option to use time mode insted of number of inputs (default)', action='store_true', default=False)
     parser.add_argument('-mv', '--max_value', type=int, help='number of seconds or number of words to be typed', required='true')
-    parser.add_argument('-uw', '--use_words', type=bool, help='Use the words mode instead of the characters (default)')
+    parser.add_argument('-uw', '--use_words', help='Use the words mode instead of the characters (default)', action='store_true', default=False)
     
-    #Create the vars to store or execute the arguments
+    #Creating the vars to store or execute the arguments
     args = vars (parser.parse_args())
 
-    if args['use_time_mode']:
+    #Creating vars to readability
+    timeMode = args['use_time_mode']
+    maxValue = args['max_value']
+    useWords = args['use_words']
+
+    #Creating the strings to show in the introduction
+    if timeMode:
         modeString='time mode'
-        numberString='have a duration of ' + str(args['max_value']) + ' seconds'
+        numberString='have a duration of ' + str(maxValue) + ' seconds'
     else:
         modeString='number of inputs mode'
-        numberString='consist in a number of ' + str(args['max_value']) + 'levels'
+        numberString='consist in a number of ' + str(maxValue) + 'levels'
 
-    if args['use_words']:
+    if useWords:
         modeString2='type words'
     else:
         modeString2='type single characters'
 
+    #Printing the introduction of the program
     print('Initializing program in '+ modeString)
     print('The game will '+ numberString)
     print('Your objective is to ' +modeString2 + ' the quickest and most correctly as possible')
     print('When you are ready, press [ENTER] to start the challenge')
     
+    #Cicle to wait for the character and run the main program
     while True:
         k = readkey()
-        if k==key.ENTER:
-            runProgram()
+        if k == key.ENTER:
+            runProgram(timeMode,maxValue,useWords)
+            break
 
 if __name__ == "__main__":
     main()
--- program_run.py
+++ program_run.py
@@ -1,4 +1,75 @@
 #!/usr/bin/env python3
 
-def runProgram():
-    print('Program running')
+#Import section
+import random
+import readchar #read keys
+from time import time, ctime
+
+from readchar import readkey, key #read keys
+
+#main program function
+def runProgram(timeMode, maxValue, useWords):
+
+    #Get a list of words
+    wordFile = "palavras.txt"
+    WORDS = open(wordFile).read().splitlines()
+
+    #main vars of completetion reseted
+    startTime = time()
+    wordWritten = 0
+    spacePressed = False
+
+    #Check if its time mode or words mode
+    if timeMode:
+        conditionEnd='Time'
+    else:
+        conditionEnd='Words'
+    
+    #Main logic of the program
+    if useWords:
+        while True: #Mode of words
+
+            randomWord = random.choice(WORDS)   #Generate the word
+            size=len(randomWord)                #Get the lenght of the word
+            print(randomWord)                   #Show the word
+            
+            for i in range(0,size):             #Cicle to wait for the whole word be typed
+                k = readkey()
+                
+                if k == " ":                    #Note: Spacebar has to interrupt the for cicle before the while cicle
+                    spacePressed = True
+                    break
+            
+            #Increase in the completition vars
+            nowTime = time()
+            wordWritten += 1
+
+            #Verify if the completition conditions were satisfied
+            if conditionEnd == 'Time' and nowTime - startTime >= maxValue:
+                break
+            if conditionEnd == 'Words' and wordWritten >= maxValue:
+                break
+            if spacePressed:
+                break
+
+    else:
+        while True: #Mode of chars
+
+            randomChar = random.choice('abcdefghijklmnopqrstuvwxyz')    #Generate the char
+            print(randomChar)                                           #Show the char
+            k = readkey()                                               #Wait for the typing
+
+            #Increase in the completition vars            
+            nowTime = time()
+            wordWritten += 1
+
+            #Verify if the completition conditions were satisfied
+            if conditionEnd == 'Time' and nowTime - startTime >= maxValue:
+                break
+            if conditionEnd == 'Words' and wordWritten >= maxValue:
+                break
+            if k == " ":
+                break
+
+    print('Programa terminado')
+    return
